755ebf8ee7d8005252c185da532480e6acb803f4
Paweł Chudecki <pawelc29@interia.pl>
2023-11-28 09:54:00 +0100

    Update footer.jsx

--- src/footer.jsx
+++ src/footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer>
       Challenge by
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+      <a href="https://www.frontendmentor.io/challenges/3column-preview-card-component-pH92eAR2-" target="_blank">
         Frontend Mentor
       </a>
       . Coded by
